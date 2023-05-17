81ff3e8d5168e4b80d0e55efd7920cdbd8054e83
matt <matt-hope@hotmail.com>
2023-05-17 15:43:19 -0400

    adding postgres connection

---
+++ db/db.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func Init() *gorm.DB {
+	fmt.Println("db init called....")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(".env file could not be loaded")
+	}
+
+	host := os.Getenv("DBHOST")
+	user := os.Getenv("DBUSER")
+	pass := os.Getenv("DBPASS")
+	dbName := os.Getenv("DBNAME")
+	port := os.Getenv("DBPORT")
+	dbURL := "host=" + host + " user=" + user + "password=" + pass + " dbname=" + dbName + "port=" + port + "sslmode=disable"
+	fmt.Println("dbURL: ", dbURL)
+	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return db
+}
