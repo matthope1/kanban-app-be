68c7a2b6d2a0bfb55b3bd4a33b7082a3fcc2ffa5
matt <matt-hope@hotmail.com>
2023-05-25 14:54:18 -0400

    apply middleware to specific routes

--- main.go
+++ main.go
@@ -18,32 +18,16 @@ func main() {
 
 	router := mux.NewRouter()
 
-	router.HandleFunc("/ping", h.Ping).Methods(http.MethodGet)
 	router.HandleFunc("/allBoards", h.GetAllBoards).Methods(http.MethodGet)
 	router.HandleFunc("/addBoard", h.AddBoard).Methods(http.MethodPost)
 	router.HandleFunc("/addColumn", h.AddColumn).Methods(http.MethodPost)
-	router.HandleFunc("/allTasks", h.AddColumn).Methods(http.MethodPost)
 
 	router.Use(middleware.LoggingMiddleware)
-	// ensure that the user is logged in and has valid jwt
-	// TODO: how can we apply this only to certain endpoints?
-	// maybe inside of the middleware we can check to see the route that was used, if its a protected route send it to the 
-	// according middleware, otherwise send it back to the normal handler
-	router.Use(middleware.EnsureValidToken)
 
+	// applying middleware to specific routes 
+	api := router.PathPrefix("").Subrouter()
+	api.Use(middleware.EnsureValidToken)
+	api.HandleFunc("/ping", h.Ping).Methods(http.MethodGet)
 
-	// router.HandleFunc("/test", middleware.EnsureValidToken()(
-	// 	http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	// 		w.Header().Set("Content-Type", "application/json")
-
-	// 		token := r.Context().Value(jwtmiddleware.ContextKey{}).(*validator.ValidatedClaims)
-
-	// 		w.Write([]byte(`{"message":"Hello from a private endpoint! You need to be authenticated to see this."}`))
-	// 	}),
-	// ))
-
-
-	// router.GET("/allBoards")
 	http.ListenAndServe("localhost:8080", router)
-
 }
