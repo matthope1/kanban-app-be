6f3708fff70ef808fb3ed53acc3f9f7c9751f7ab
matt <matt-hope@hotmail.com>
2023-06-15 22:11:48 -0400

    println debugs

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -89,6 +89,7 @@ func (c CustomClaims) HasScope(expectedScope string) bool {
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Do stuff here
+		log.Println("logging middleware..... ")
 		log.Println(r.RequestURI)
 		// Call the next handler, which can be another middleware in the chain, or the final handler.
 		next.ServeHTTP(w, r)
